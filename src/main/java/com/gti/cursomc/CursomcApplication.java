fbb7237a8f3fbd3ca8d743160a118b0836475b76
Gullit <Gullit@DESKTOP-E78PP5Q>
2018-10-09 21:54:23 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/gti/cursomc/CursomcApplication.java
+++ src/main/java/com/gti/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.gti.cursomc.domain.Cidade;
 import com.gti.cursomc.domain.Cliente;
 import com.gti.cursomc.domain.Endereco;
 import com.gti.cursomc.domain.Estado;
+import com.gti.cursomc.domain.ItemPedido;
 import com.gti.cursomc.domain.Pagamento;
 import com.gti.cursomc.domain.PagamentoComBoleto;
 import com.gti.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.gti.cursomc.repositories.CidadeRepository;
 import com.gti.cursomc.repositories.ClienteRepository;
 import com.gti.cursomc.repositories.EnderecoRepository;
 import com.gti.cursomc.repositories.EstadoRepository;
+import com.gti.cursomc.repositories.ItemPedidoRepository;
 import com.gti.cursomc.repositories.PagamentoRepository;
 import com.gti.cursomc.repositories.PedidoRepository;
 import com.gti.cursomc.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -122,5 +127,18 @@ public class CursomcApplication implements CommandLineRunner {
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.0, 1, 2000.0);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.0, 2, 80.0);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.0, 1, 800.0);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
