71499a86e7d6b2ddc7fbb97312d6467344dd9da4
Gullit <Gullit@DESKTOP-E78PP5Q>
2018-10-24 20:36:08 -0300

    Inserindo uma nova Categoria om POST

--- src/main/java/com/gti/cursomc/services/CategoriaService.java
+++ src/main/java/com/gti/cursomc/services/CategoriaService.java
@@ -22,4 +22,7 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	public Categoria insert(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
 }
