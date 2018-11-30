06e33d3bf6e20d755a4329aa9ac08d4e7629d7bd
812138 <812138@D-PROD-BP137481.prodesp-sede.local>
2018-11-30 16:50:57 -0200

    Salvando perfis de usuário na base de dados

--- src/main/java/com/gti/cursomc/services/DBService.java
+++ src/main/java/com/gti/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.gti.cursomc.domain.PagamentoComCartao;
 import com.gti.cursomc.domain.Pedido;
 import com.gti.cursomc.domain.Produto;
 import com.gti.cursomc.domain.enums.EstadoPagamento;
+import com.gti.cursomc.domain.enums.Perfil;
 import com.gti.cursomc.domain.enums.TipoCliente;
 import com.gti.cursomc.repositories.CategoriaRepository;
 import com.gti.cursomc.repositories.CidadeRepository;
@@ -120,16 +121,21 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Maria Silva", "gullit.menezes@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("batata"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Ferreira", "gulyt_vp@hotmail.com", "20842157816", TipoCliente.PESSOAFISICA, bCryptPasswordEncoder.encode("batata"));
+		cli2.addPerfis(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("32654654", "96567964"));
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "423809427", cli2, c2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 	
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
